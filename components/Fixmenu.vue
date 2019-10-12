24b4eb48cf11d82282ab435bfe5a74b75435a6c9
shirai ryohei <shirai.ryohei0302@gmail.com>
2019-10-12 18:00:55 +0900

    fixmenuを追加

---
+++ components/Fixmenu.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="c-fixmenu">
+    <nuxt-link to="/" class="_item">
+      <i class="fas fa-home"></i>
+    </nuxt-link>
+    <span class="_item">
+      <i class="fas fa-lightbulb"></i>
+    </span>
+    <span class="_item">
+      <i class="fas fa-stream"></i>
+    </span>
+    <span class="_item" @click="toPagetop">
+      <i class="fas fa-level-up-alt"></i>
+    </span>
+  </div>
+</template>
+
+<script>
+import smoothscroll from 'smoothscroll-polyfill'
+
+export default {
+  mounted() {
+    smoothscroll.polyfill()
+  },
+  methods: {
+    toPagetop: () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      })
+    }
+  }
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -5,17 +5,21 @@
     <nuxt />
 
     <Footer />
+
+    <Fixmenu />
   </div>
 </template>
 
 <script>
 import Header from '~/components/Header.vue'
 import Footer from '~/components/Footer.vue'
+import Fixmenu from '~/components/Fixmenu.vue'
 
 export default {
   components: {
     Header,
-    Footer
+    Footer,
+    Fixmenu
   }
 }
 </script>
