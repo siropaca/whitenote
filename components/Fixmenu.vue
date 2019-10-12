597bb8b43ebebf466d1850474acbf27de2004b99
shirai ryohei <shirai.ryohei0302@gmail.com>
2019-10-12 22:51:23 +0900

    fixmenuを修正

--- components/Fixmenu.vue
+++ components/Fixmenu.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="c-fixmenu">
-    <nuxt-link to="/" class="_item">
-      <i class="fas fa-home"></i>
-    </nuxt-link>
-    <span class="_item">
-      <i class="fas fa-lightbulb"></i>
-    </span>
-    <span class="_item">
-      <i class="fas fa-stream"></i>
-    </span>
-    <span class="_item" @click="toPagetop">
-      <i class="fas fa-level-up-alt"></i>
-    </span>
+  <div v-show="showMenu" class="c-fixmenu">
+    <div class="o-inner -s">
+      <div class="_contents">
+        <nuxt-link to="/" class="_item">
+          <i class="fas fa-home"></i>
+        </nuxt-link>
+        <span class="_item">
+          <i class="fas fa-lightbulb"></i>
+        </span>
+        <span class="_item">
+          <i class="fas fa-stream"></i>
+        </span>
+        <span class="_item" @click="toPagetop">
+          <i class="fas fa-level-up-alt"></i>
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,8 +23,23 @@
 import smoothscroll from 'smoothscroll-polyfill'
 
 export default {
+  data: () => {
+    return {
+      position: 0,
+      showMenu: true
+    }
+  },
+  watch: {
+    position(newPosi, oldPosi) {
+      this.showMenu = newPosi < oldPosi || false
+    }
+  },
   mounted() {
     smoothscroll.polyfill()
+
+    window.addEventListener('scroll', () => {
+      this.position = window.scrollY
+    })
   },
   methods: {
     toPagetop: () => {
