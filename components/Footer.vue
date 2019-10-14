b92416a8d103b4adb9c63aa0ba8d0365be12ebc5
shirai ryohei <shirai.ryohei0302@gmail.com>
2019-10-14 15:50:35 +0900

    著作権追加

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,10 +2,18 @@
   <footer class="c-footer">
     <div class="o-inner -m">
       <div class="_socials">
-        <i class="fab fa-github"></i>
-        <i class="fab fa-twitter"></i>
-        <i class="fab fa-instagram"></i>
+        <a href="https://github.com/siropaca" target="_blank">
+          <i class="fab fa-github"></i>
+        </a>
+        <a href="https://twitter.com/siropaca" target="_blank">
+          <i class="fab fa-twitter"></i>
+        </a>
+        <a href="https://www.instagram.com/siro.pac.a/" target="_blank">
+          <i class="fab fa-instagram"></i>
+        </a>
       </div>
+
+      <div class="_copyright">©︎ 2019 All rights reserved - Siropaca</div>
     </div>
   </footer>
 </template>
